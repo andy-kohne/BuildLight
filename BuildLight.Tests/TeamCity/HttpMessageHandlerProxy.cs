01e83cbef5ead1099f33abdd0c2524aaa2e1436a
Andy Kohne <andy-kohne@users.noreply.github.com>
2017-10-05 19:03:31 -0400

    Add tests project, and tests for TeamCityApiClient

---
+++ BuildLight.Tests/TeamCity/HttpMessageHandlerProxy.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BuildLight.Tests.TeamCity
+{
+    public interface IFakeMessageHandler
+    {
+        Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken);
+    }
+
+    public class HttpMessageHandlerProxy : HttpMessageHandler
+    {
+        private readonly IFakeMessageHandler _fakeHandler;
+
+        public HttpMessageHandlerProxy(IFakeMessageHandler fakeHandler)
+        {
+            _fakeHandler = fakeHandler;
+        }
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return _fakeHandler.SendAsync(request, cancellationToken);
+        }
+    }
+}
