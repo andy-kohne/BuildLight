c74d80d0afcf8d07398860a411d19b208ed0f4b2
Andy Kohne <andy-kohne@users.noreply.github.com>
2018-01-23 13:35:48 -0500

    use separate tasks to prevent deadlock

--- BuildLight.UWP/MainPage.xaml.cs
+++ BuildLight.UWP/MainPage.xaml.cs
@@ -22,9 +22,12 @@ namespace BuildLight.UWP
             InitializeComponent();
             _cancellationToken = new CancellationToken();
 
-            var settings = GetSettingsAsync().Result;
+            var settingsTask = Task.Run(GetSettingsAsync);
+            var pwmcontrollerTask = Task.Run(PwmControllerProxy.GetGontroller);
+
+            var pwmController =pwmcontrollerTask.Result;
+            var settings = settingsTask.Result;
             var tcApiClient = new TeamCityApiClient(settings.Host, settings.UserName, settings.Password);
-            var pwmController = PwmControllerProxy.GetGontroller().Result;
 
             _visualizationService = new VisualizationService(settings.Visualizations, pwmController);
             _visualizationService.Run(_cancellationToken);
